9e65c77c385ad2c4b701a1aaf8ab78a720a066fd
entlaabstudio <entlaab.studio@gmail.com>
2019-10-12 16:57:45 +0200

    Přidání nových parametrů a interakce mezi planetami

--- js/engines/energon.js
+++ js/engines/energon.js
@@ -25,19 +25,29 @@ class EnergonEngine extends DigitalifeBase {
 	getMoving() {
 		// console.log("už to jede");
 		var ostatni = super.popDataOthers();
+
 		// console.log("já objekt X",this.params.posX);
 		// console.log("já objekt Y",this.params.posY);
 		// console.log("já objekt váha",this.params.weight);
+		
+
 		for (var prop in ostatni) {
-			// console.log("var prop",prop);
-			// console.log("hodnoty X",super.popDataOthers()[prop].params.posX);
-			// console.log("hodnoty Y",super.popDataOthers()[prop].params.posY);
-			// console.log("váha",super.popDataOthers()[prop].params.weight);
 			/**
 			 * Následuje experimentální kód, který projde výpočetním zdokonalením (Energon)
 			 */
-			this.params.posX += 1;
-			this.params.posY -= 1;
+			
+			// console.log("váha",super.popDataOthers()[prop].params.weight);
+			var otherPosX = super.popDataOthers()[prop].params.posX;
+			var otherPosY = super.popDataOthers()[prop].params.posY;
+			
+			var accXChange = ((otherPosX > this.params.posX) ? this.params.acclrX += 1 : this.params.acclrX -= 1);
+			var accYChange = ((otherPosY > this.params.posY) ? this.params.acclrY += 1 : this.params.acclrY -= 1);
+			
+			this.params.acclrX = accXChange;
+			this.params.acclrY = accYChange;
+			
+			this.params.posX += accXChange;
+			this.params.posY += accYChange;
 		}
 		/**
 		 * Uložení dat
