d0c7de17366361e5713a23e2a18d51397734e24d
Robert Němec <engine-r@seznam.cz>
2023-09-22 04:40:52 +0200

    Runtime...

--- webserver/content/web/_digitalif3/js/Digitalif3.js
+++ webserver/content/web/_digitalif3/js/Digitalif3.js
@@ -14,6 +14,7 @@ class Digitalif3 {
 
     runtime() {
         var c = this.console;
+        var cfg = this.conf;
         c.info("aahoj");
         c.info(this.conf)
         c.info(this.dlParams.objekty);
@@ -22,9 +23,21 @@ class Digitalif3 {
         for (const [key, value] of Object.entries(this.dlParams.objekty)) {
             console.log(key,value);
             this.runtimeTicker = [];
-            this.runtimeTicker[i] = setInterval(function() {
-                console.log(key);
-            },200000 / value.fps); // TODO: ),1000 / value.fps);
+            this.runtimeTicker[i] = setInterval(
+                function() {
+                    if (!cfg.app.settings.global.paused) {
+                        if (cfg.app.settings.global.zeroPoints.consoleInfo) {
+                            c.info(
+                                value.fps + 'fps of object ' + key + ' | ' +
+                                cfg.app.settings.global.fpsLimit + 'fps global limit' + ' | ' +
+                                cfg.app.settings.global.slowdownMultiplier + "x slower"
+                            );
+                        }
+                    }
+                }, (
+                    1000 / value.fps >= 1000 / cfg.app.settings.global.fpsLimit ? 1000 / value.fps : 1000 / cfg.app.settings.global.fpsLimit
+                ) * cfg.app.settings.global.slowdownMultiplier
+            );
             i++;
         }
     }
